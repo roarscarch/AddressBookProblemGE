0d21bcae8c408eafd5170fb62fab9e5840142155
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-29 17:59:16 +0530

    added UC3

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -39,4 +39,38 @@ public class AddressBook {
         }
 
     }
+
+    public void editContact() {
+
+        System.out.println("Enter the First Name of Contact to Edit : ");
+        String FirstName = (sc.next());
+        boolean IsAvaible = false;
+
+        for (Contact contact : contactArrayList) {
+            if (contact.getFirstName().equals(FirstName)) {
+                IsAvaible = true;
+
+                System.out.println("Enter the First Name : ");
+                contact.setFirstName(sc.next());
+                System.out.println("Enter the Last Name : ");
+                contact.setLastName(sc.next());
+                System.out.println("Enter the Address : ");
+                contact.setAddress(sc.next());
+                System.out.println("Enter the City : ");
+                contact.setCity(sc.next());
+                System.out.println("Enter the State : ");
+                contact.setState(sc.next());
+                System.out.println("Enter the Zipcode : ");
+                contact.setZipCode(sc.nextInt());
+                System.out.println("Enter the Mobile Number : ");
+                contact.setMobileNo(sc.nextInt());
+                System.out.println("Enter the Email Id : ");
+                contact.setEmailID(sc.next());
+            }
+        }
+
+        if (IsAvaible == false) {
+            System.out.println("CONTACT NOT AVAIBLE");
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,32 +5,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Welcome to address book compulation program");
+        System.out.println("Welcome to address book computation program");
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            AddressBook addressBook = new AddressBook();
+        AddressBook addressBook = new AddressBook();
 
-            while (true) {
-                System.out.println("0.Exit \n1.Add Contact \n2.Display Contact ");
-                System.out.println("Enter the Operation Number : ");
-                int operationNumber = sc.nextInt();
+        while (true) {
+            System.out.println("0.Exit \n1.Add Contact \n2.Display Contact \n3.Edit ");
+            System.out.println("Enter the Operation Number : ");
+            int operationNumber = sc.nextInt();
 
-                switch (operationNumber) {
+            switch (operationNumber) {
 
-                    case 0:
-                        System.exit(0);
-                        break;
-                    case 1:
-                        addressBook.addContact();
-                        break;
-                    case 2:
-                        addressBook.displayContact();
-                        break;
-                    default:
-                        System.out.println("Invalid Input");
-                }
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.displayContact();
+                    break;
+                case 3:
+                    addressBook.editContact();
+                    break;
+                default:
+                    System.out.println("Invalid Input");
             }
         }
-
+    }
 }
